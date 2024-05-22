96ca8b0b2463b02e8db12eb6a90adf84fb0f7206
MarcosRamonLorenzo$ git config --get-all user.name <marcosramon.alu@iespacomolla.es>
2024-05-22 23:12:46 +0200

    add res status comments controller

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -4,7 +4,7 @@ const { handleErr } = require("@errors/errorHandler.js");
 const getCommentsByServiceId = async (req, res) => {
   try {
     const comments = await Comment.getCommentsByServiceId(req.params.serviceId);
-    res.status(200).json(comments);
+    res.status(200).json({ status: "successful ", data: comments, error: null });
   } catch (error) {
     console.log(error);
     handleErr(error);
@@ -14,7 +14,7 @@ const getCommentsByServiceId = async (req, res) => {
 const getCommentByUserId = async (req, res) => {
   try {
     const comments = await Comment.getCommentByUserId(req.params.userId);
-    res.status(200).json(comments);
+    res.status(200).json({ status: "successful ", data: comments, error: null });
   } catch (error) {
     handleErr(error);
   }
@@ -23,7 +23,7 @@ const getCommentByUserId = async (req, res) => {
 const create = async (req, res) => {
   try {
     const newComment = await Comment.create(req.body);
-    res.status(201).json(newComment);
+    res.status(201).json({ status: "successful ", data: newComment, error: null });
   } catch (error) {
     handleErr(error);
   }
@@ -32,7 +32,7 @@ const create = async (req, res) => {
 const deleteComment = async (req, res) => {
   try {
     const deletedComment = await Comment.deleteComment(req.params.id);
-    res.status(200).json(deletedComment);
+    res.status(200).json({ status: "successful ", data: deleteComment, error: null });
   } catch (error) {
     handleErr(error);
   }
